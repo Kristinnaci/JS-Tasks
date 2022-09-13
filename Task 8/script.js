df5f0e224439d3c13c3d1d27952e7477a5198d4c
Kristina <kristinacickevice@gmail.com>
2022-09-13 18:41:05 +0300

    update

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,17 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+function calculator(a, b) {
+  this.sum = a + b;
+  this.subtraction = a - b;
+  this.multiplication = a * b;
+  this.division = a / b;
+  this.print = function () {
+    console.log(this.sum + " sum");
+    console.log(this.subtraction + " subtraction");
+    console.log(this.multiplication + " multiplication");
+    console.log(this.division + " division");
+  };
+}
+const input = new calculator(5, 2);
+input.print();
--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,21 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+class movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+    this.costs = () => {
+      if (this.budget > 100000000) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+  }
+}
+
+const wasExpensive = new movie("GirlIntheTrain", "TateTaylor", 45000000);
+
+console.log(wasExpensive);
